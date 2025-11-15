6fde6c4a6b696d8307c1c3ef4edfd25569fc1c93
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-11-15 21:54:37 +0400

    Minor: Add more meaningful error log
    Co-authored-by: phoenix0x1 <221748789+phoenix0x1@users.noreply.github.com>

--- runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
+++ runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.0 (trial), 000000, 2025-11-09T06:57:58.560067
+# Pyarmor 9.2.0 (trial), 000000, 2025-11-15T17:52:32.310548
 from .pyarmor_runtime import __pyarmor__
--- runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.0 (trial), 000000, 2025-11-09T06:58:10.062050
+# Pyarmor 9.2.0 (trial), 000000, 2025-11-15T17:53:19.617474
 from .pyarmor_runtime import __pyarmor__
